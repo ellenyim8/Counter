15190da5effa25489f22f0d2be9d6d8f6975a55f
Ellen <69826832+ellenyim8@users.noreply.github.com>
2021-12-18 22:46:53 -0800

    Add files via upload
    example output

---
+++ z_MomCounter_output.h
@@ -0,0 +1,70 @@
+#ifndef Z_MOMCOUNTER_OUTPUT_H
+#define Z_MOMCOUNTER_OUTPUT_H
+
+/*
+-------------------ALL RESULTS ARE VERIFIED-------------------
+--------------------------NORMAL RUN--------------------------
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :f
+0001
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :f
+0002
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :f
+0003
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :d
+0013
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :d
+0023
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :d
+0033
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :s
+0133
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :s
+0233
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :s
+0333
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+1333
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :s
+1433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+2433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+3433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+4433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+5433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+6433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+7433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+8433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+9433
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+MCounter is in state of error: INVALID COUNT
+OVERFLOW
+
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :d
+MCounter is in state of error: INVALID COUNT
+OVERFLOW
+
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :r
+0000
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :f
+0001
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :d
+0011
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :s
+0111
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :a
+1111
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :s
+1211
+MENU: [a] 1000 | [s] 100 | [d] 10 | [f] 1 | [r]eset | e[x]it :x
+----------------------END----------------------
+Press <RETURN> to close this window...
+
+*/
+#endif // Z_MOMCOUNTER_OUTPUT_H
